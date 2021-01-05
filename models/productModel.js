e3fb1440cdc1a9fa5f56b156dbfd0607e1e2d41e
dansantander <dansantandr@gmail.com>
2021-01-05 16:00:24 -0600

    feat: add single product get method

--- models/productModel.js
+++ models/productModel.js
@@ -1,11 +1,23 @@
 const products = require('../data/products.json');
 
+// It's not technically necessary to return promises
+// but we do it in order to respect API's way of working
+// when using data
+
 function findAll() {
   return new Promise((resolve, reject)=> {
     resolve(products);
   })
 }
 
+function findById(id) {
+  const product = products.find( p => p.id === id )
+  return new Promise((resolve, reject)=> {
+    resolve(product);
+  })
+}
+
 module.exports = {
-  findAll
+  findAll,
+  findById
 }
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http');
-const { getProducts } = require('./controllers/productController');
+const { getProducts, getProduct } = require('./controllers/productController');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/api/products' && req.method === 'GET') { 
     getProducts(req, res);
+  } else if (req.url.match(/\/api\/products\/[0-9]+/) && req.method === 'GET') { 
+    // the url is /api/products/number
+    // so in order to get the id, we split the string by the '/'
+    // that will return the array ['', 'api', 'products', 'number']
+    // so then we grab just the number which is in the 3rd position of the array
+    const id = req.url.split('/')[3];
+    getProduct(req, res, id);
   } else {
     res.writeHead(404, { "Content-Type": "application/html" });
     res.end(JSON.stringify({ message: "404 Not Found" }));
